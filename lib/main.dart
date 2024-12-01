d34fa497a7977b94d763112d98229addc2a983c9
Data-MaSTeRR <hyunwoo8504@gmail.com>
2024-12-02 04:36:59 +0900

    Feat: 3초마다 비동기처리 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // Timer를 사용하기 위한 import
 import 'routes.dart';
 import 'UserData.dart';
 
@@ -8,15 +9,18 @@ void main() async {
   final userService = UserService(); // Singleton 인스턴스 생성
   await userService.fetchAllData(); // 사용자 및 포스트 데이터 초기화
 
-  runApp(HyFiveApp());
+  runApp(HyFiveApp(userService: userService)); // UserService를 전달
 }
 
 class HyFiveApp extends StatelessWidget {
+  final UserService userService;
+
+  HyFiveApp({required this.userService}); // UserService를 생성자에서 받음
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 디버그 표식 제거
-
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
@@ -40,9 +44,56 @@ class HyFiveApp extends StatelessWidget {
           ),
         ),
       ),
-
       initialRoute: '/home', // 홈 페이지를 Home으로 설정
       routes: routes,
+      builder: (context, child) {
+        return AppLifecycleObserver(
+          userService: userService,
+          child: child!,
+        );
+      },
     );
+  }
+}
+
+class AppLifecycleObserver extends StatefulWidget {
+  final Widget child;
+  final UserService userService;
+
+  AppLifecycleObserver({required this.child, required this.userService});
+
+  @override
+  _AppLifecycleObserverState createState() => _AppLifecycleObserverState();
+}
+
+class _AppLifecycleObserverState extends State<AppLifecycleObserver>
+    with WidgetsBindingObserver {
+  Timer? _syncTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _startDataSync();
+  }
+
+  void _startDataSync() {
+    // 주기적으로 데이터 갱신 (예: 10초마다)
+    _syncTimer = Timer.periodic(Duration(seconds: 3), (timer) async {
+      await widget.userService.fetchAllData();
+      print("데이터 동기화 완료");
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _syncTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
